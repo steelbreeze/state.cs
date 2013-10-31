de70694c2b4ad906d9ea9e4290297e682c12f0e4
Mesmo <mesmo@steelbreeze.net>
2013-10-31 22:58:16 +0000

    Remove a few redundant using statements

--- src/PseudoState.cs
+++ src/PseudoState.cs
@@ -15,8 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 
 namespace Steelbreeze.Behavior
 {
