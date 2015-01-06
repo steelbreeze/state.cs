b0386daa2019d61148e73188f8444e0d9ff10634
David Mesquita-Morris <mesmo@steelbreeze.net>
2015-01-06 20:33:54 +0000

    A little re-factoring
    Rename the templates versions of DictionaryContext and XContext to
    DictionaryContextBase and XContextBase respectively

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to state.cs
 
-The current stable release is 5.1.1.
+The current stable release is 5.1.2.
 
 If you're using state.cs I'd love to hear about it; please e-mail me at mesmo@steelbreeze.net 
 
--- src/XContextBase.cs
+++ src/XContextBase.cs
@@ -16,7 +16,7 @@ namespace Steelbreeze.Behavior.StateMachines {
 	/// Note that properties and methods have been explicitly implemented to hide the members from use other than via the IContext interface.
 	/// Should you need persistence, or other such behaviour relating to the context class, implement another class implementing IContext.
 	/// </remarks>
-	public abstract class XContext<TContext> : IContext<TContext> where TContext : IContext<TContext> {
+	public abstract class XContextBase<TContext> : IContext<TContext> where TContext : IContext<TContext> {
 		/// <summary>
 		/// The XElement that stores the context information.
 		/// </summary>
@@ -29,7 +29,7 @@ namespace Steelbreeze.Behavior.StateMachines {
 		/// Creates a new instance of the XmlContext class.
 		/// </summary>
 		/// <param name="content">Any additional XML structure that you may need under the root element.</param>
-		public XContext( params object[] content ) {
+		public XContextBase( params object[] content ) {
 			this.XElement = new XElement( "stateMachineContext", content );
 
 			this.XElement.Add( new XAttribute( "terminated", false ) );
@@ -90,9 +90,4 @@ namespace Steelbreeze.Behavior.StateMachines {
 			}
 		}
 	}
-
-	/// <summary>
-	/// A sample of an object to extend as a base for a state machine context objects based on an XElement.
-	/// </summary>
-	public class XContext : XContext<XContext> { }
 }
